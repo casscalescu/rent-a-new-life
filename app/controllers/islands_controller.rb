ac16b301fddd2d6f7a2fc4b76471445c1c511c72
Cassandra Calescu <casscalescu@gmail.com>
2022-07-28 09:07:34 +0300

    Show page

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,5 +1,6 @@
 class IslandsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :find_island, only: %i[edit update show destroy]
 
   def index
     @islands = Island.all
@@ -23,15 +24,25 @@ class IslandsController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    if @island.update(island_params)
+      redirect_to island_path
+    else
+      render :new
+    end
   end
 
   def destroy
   end
 
   private
+
+  def find_island
+    @island = Island.find(params[:id])
+  end
 
   def island_params
     params.require(:island).permit(:name, :location, :price_per_night, :description, :longitude, :latitude, :photos)
