3c80079f842162534b8786aeea97c409a34a92c2
Cassandra Calescu <casscalescu@gmail.com>
2022-07-28 10:11:17 +0300

    Added map

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -4,6 +4,13 @@ class IslandsController < ApplicationController
 
   def index
     @islands = Island.all
+    @markers = @islands.geocoded.map do |island|
+      {
+        lat: island.latitude,
+        lng: island.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {island: island})
+      }
+    end
   end
 
   def new
--- app/models/island.rb
+++ app/models/island.rb
@@ -2,14 +2,17 @@ class Island < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   CATEGORY = %w(Tidal Barrier Oceanic Coral Continental Artificial)
 
-  validates :name, presence: true
-  validates :location, presence: true
-  validates :description, presence: true
-  validates :category, presence: true
-  validates :price_per_night, presence: true, numericality: true
-  validates :deposit, presence: true, numericality: true
+  # validates :name, presence: true
+  # validates :location, presence: true
+  # validates :description, presence: true
+  # validates :category, presence: true
+  # validates :price_per_night, presence: true, numericality: true
+  # validates :deposit, presence: true, numericality: true
 
   def category
     CATEGORY
